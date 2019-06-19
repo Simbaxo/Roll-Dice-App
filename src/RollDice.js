0e7b10e57a1cb6ddb268b494068602545e547a61
Andres Mena <ballerlikemike23@gmail.com>
2019-06-18 23:41:40 -0400

    Correct Roll Dice Component

--- src/RollDice.js
+++ src/RollDice.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react'
 import Die from './Die'
 
 class RollDice extends Component {
+  static defaultProps = {
+    sides: ['one', 'two', 'three', 'four', 'five', 'six']
+  }
+
   constructor(props) {
     super(props)
-    this.state = { value: 'one' }
+    this.state = { die1: 'one', die2: 'one' }
     this.roll = this.roll.bind(this)
   }
 
   roll(e) {
-    let nums = ['two', 'three', 'four', 'five', 'six']
-    let rand = nums[Math.floor(Math.random * nums.length)]
-    this.setState({ value: rand })
+    // pick two new rolls
+    const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+    const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
+    //set state with new rolls
+    this.setState({ die1: newDie1, die2: newDie2 })
   }
 
   render() {
     return (
       <div>
-        <h1>This is working!</h1>
-        {
-
-        }
-        <Die value={this.state.value} />
-        <Die value={this.state.value} />
+        <Die face={this.state.die1} />
+        <Die face={this.state.die2} />
         <button onClick={this.roll}>Roll Dice!</button>
       </div>
     )
